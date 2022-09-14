6b3870283ef6e73138b10ac9784ca71648853a27
PanJakub97 <kuba.stasiak78@gmail.com>
2022-09-14 22:03:59 +0200

    Course Manager

---
+++ CourseManager/Models/TeacherModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManager.Models
+{
+    internal class TeacherModel
+    {
+        public int TeacherId { get; set; }
+        public string Degre { get; set; }
+        public string Pseudo { get; set; }
+    }
+}
---
+++ CourseManager/Repository/TeacherCommand.cs
@@ -0,0 +1,35 @@
+﻿using CourseManager.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManager.Repository
+{
+    internal class TeacherCommand
+    {
+        private string _connectionString;
+
+        public TeacherCommand(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IList<TeacherModel> GetList()
+        {
+            List<TeacherModel> teachers = new List<TeacherModel>();
+
+            var sql = "Teacher_GetList";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                teachers = connection.Query<TeacherModel>(sql).ToList();
+            }
+
+            return teachers;
+        }
+    }
+}
